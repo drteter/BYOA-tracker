bb56363bddfad8da2d6c5a892431c59f3a855552
David Teter <davidteter@Davids-MacBook-Air.local>
2025-02-03 11:35:41 -0500

    Fix Babel configuration for Expo SDK 50+

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'expo-router/babel',
       '@babel/plugin-transform-runtime',
       '@babel/plugin-transform-export-namespace-from',
       '@babel/plugin-proposal-export-namespace-from'
